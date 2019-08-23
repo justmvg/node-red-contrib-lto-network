16d93e68ddd276e2fcc38f6636613d312b79a660
Michael van G <michael@Michaels-MacBook-Pro.local>
2019-08-23 17:01:56 +0200

    Seperated all nodes

---
+++ nodes/get-active-leases/get-active-leases.js
@@ -0,0 +1,53 @@
+var request = require('request');
+var request = request.defaults({ jar: true })
+var defaultAPI = "https://nodes.lto.network";
+
+module.exports = function (RED) {
+    
+    function GetActiveLeases(config) {
+        RED.nodes.createNode(this, config);
+
+        var node = this;
+        node.on('input', function (msg) {
+
+            if (typeof config.endpoint === "undefined" || config.endpoint == "") {
+                config.endpoint = defaultAPI;
+            }
+            
+            if (typeof config.address === "undefined") {
+                msg.payload = 'No LTO Wallet Address configured.';
+                this.status({ fill: "red", shape: "dot", text: msg.payload });
+                node.send(msg);
+            } else {
+                var ltoEndpoint = config.endpoint;
+                var ltoAddress = config.address;
+
+                    if (typeof msg.payload.address !== "undefined" && msg.payload.address != "") {
+                        ltoAddress = msg.payload.address;
+                    }
+                    if (ltoAddress == null) {
+                        msg.payload = 'LTO Wallet Address is empty.';
+                        this.status({ fill: "red", shape: "dot", text: msg.payload });
+                        node.send(msg);
+                    }
+                    var endpoint = ltoEndpoint + '/leasing/active/' + ltoAddress
+
+                    request.get(endpoint, (err, response, body) => {
+                        if (err) {
+                            return console.error('Failed:', err);
+                        }
+                        var body2json = JSON.parse(body);
+                        if (typeof body2json.error === "undefined") {
+                            this.status({ fill: "green", shape: "dot", text: endpoint });
+                        } else {
+                            this.status({ fill: "red", shape: "dot", text: endpoint });
+                        }
+                        msg.payload = body2json;
+                        node.send(msg);
+                    });
+                
+            }
+        });
+    }
+    RED.nodes.registerType("Get active leases", GetActiveLeases);
+}
